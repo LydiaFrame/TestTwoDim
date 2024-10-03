fdd393ee7a18abd34a4c72f30590345d2284abae
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2024-10-03 20:09:31 +0000

    start not finished

---
+++ testtwodim.TestTwoDim/TestTwoDim.java
@@ -0,0 +1,15 @@
+/*
+ * Program: TestArray.java
+ * Student: Lydia Frame 
+ * Purpose: Create a two-dimensional array and print result. Upload .java file to Canvas as TestTwoDim.
+*/
+
+import java.util.Scanner; 
+
+public class TestTwoDim {
+    public static void main(String[] args){
+
+        
+
+    }
+}
